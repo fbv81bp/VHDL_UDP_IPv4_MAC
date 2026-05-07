b3a70ea9034806ea2b28f14b6b598151ededd763
Fekete Balazs Valer <48908633+fbv81bp@users.noreply.github.com>
2026-05-07 23:18:32 +0200

    Update copyright information in crc32ieeeCalc.vhd

--- UDP IPv4 MAC stack/crc32ieeeCalc.vhd
+++ UDP IPv4 MAC stack/crc32ieeeCalc.vhd
@@ -1,6 +1,6 @@
 --MIT License
 --
---Copyright (c) 2019 Balazs Valer Fekete
+--Copyright (c) 2019 Balazs Valer Fekete fbv81bp@[gmail.com|outlook.hu]
 --
 --Permission is hereby granted, free of charge, to any person obtaining a copy
 --of this software and associated documentation files (the "Software"), to deal
